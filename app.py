1d7eb917211a120ee9b67685aaeeefaa88a3f61a
zrk16 <zrkhursheed@gmail.com>
2026-04-29 08:51:14 +0800

    Fix Vercel deployment: rename spaced filenames, serve statics via CDN

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, static_folder='.', static_url_path='')
 
 @app.route('/')
 def home():
-    return app.send_static_file('Home page.html')
+    return app.send_static_file('about.html')
 
 @app.route('/<path:filename>')
 def serve_file(filename):
